5f9a41580e890c8a0c63f26d1399764d971ddc6d
Marta Prokopiv <117581510+Promart1@users.noreply.github.com>
2024-02-23 15:13:32 +0200

    add header

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,26 @@
-// const menuBtn = document.querySelector('.menu__button');
-// const menuClose = document.querySelector('.menu__close');
-// const menuList = document.querySelector('.menu__list');
-// const menuShadow = document.querySelector('.menu--close')
+const menuBtn = document.querySelector('.menu__button');
+const menuClose = document.querySelector('.menu__close');
+const menuList = document.querySelector('.menu__list');
+const menuShadow = document.querySelector('.menu--close')
 
-// menuBtn.addEventListener('click', ()=>{
+menuBtn.addEventListener('click', ()=>{
+    menuList.classList.toggle('menu__list--open');
+    menuShadow.classList.toggle('menu--open');
+    console.log("open");
+});
+
+menuClose.addEventListener('click', ()=>{
+    menuList.classList.remove('menu__list--open');
+    menuShadow.classList.remove('menu--open');
+    console.log("close");
+});
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   const menuBtn = document.querySelector('.menu__button');
+//   const menuList = document.querySelector('.menu__list');
+
+//   menuBtn.addEventListener('click', function () {
 //     menuList.classList.toggle('menu__list--open');
-//     menuShadow.classList.toggle('menu--open');
-// });
-
-// menuClose.addEventListener('click', ()=>{
-//     menuList.classList.remove('menu__list--open');
-//     menuShadow.classList.remove('menu--open');
+//   });
 // });
 
