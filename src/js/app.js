821aea89299992132d9ec954268c8351992e038a
Marta Prokopiv <117581510+Promart1@users.noreply.github.com>
2024-03-04 18:11:45 +0200

    add modal window and part of assortment section

--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,47 @@
 const menuBtn = document.querySelector('.menu__button');
 const menuClose = document.querySelector('.menu__close');
 const menuList = document.querySelector('.menu__list');
-const menuShadow = document.querySelector('.menu--close')
+const menuShadow = document.querySelector('.menu--close');
 
-menuBtn.addEventListener('click', ()=>{
+menuBtn.addEventListener('click', () => {
     menuList.classList.toggle('menu__list--open');
     menuShadow.classList.toggle('menu--open');
     console.log("open");
 });
 
-menuClose.addEventListener('click', ()=>{
+menuClose.addEventListener('click', () => {
     menuList.classList.remove('menu__list--open');
     menuShadow.classList.remove('menu--open');
     console.log("close");
 });
 
-// document.addEventListener('DOMContentLoaded', function () {
-//   const menuBtn = document.querySelector('.menu__button');
-//   const menuList = document.querySelector('.menu__list');
+menuShadow.addEventListener('click', (event) => {
+    if (!menuList.contains(event.target)) {
+        menuList.classList.remove('menu__list--open');
+        menuShadow.classList.remove('menu--open');
+        console.log("close");
+    }
+});
 
-//   menuBtn.addEventListener('click', function () {
-//     menuList.classList.toggle('menu__list--open');
-//   });
-// });
 
+
+//Modal PopUp
+
+
+var modal = document.getElementById('modalPopUp');
+var openModalBtn = document.getElementById('openModalBtn');
+var closeModalBtn = document.getElementById('closeModalBtn');
+
+openModalBtn.onclick = function() {
+  modal.style.display = 'block';
+}
+
+closeModalBtn.onclick = function() {
+  modal.style.display = 'none';
+}
+
+window.onclick = function(event) {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+}
