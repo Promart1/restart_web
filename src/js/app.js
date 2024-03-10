1b25142cd68750a6f6b370407244668bfee13764
Marta Prokopiv <117581510+Promart1@users.noreply.github.com>
2024-03-10 19:20:09 +0200

    fix bug js popup and page path

--- src/js/app.js
+++ src/js/app.js
@@ -45,19 +45,19 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-document.addEventListener('DOMContentLoaded', function() {
-  const slider = document.querySelector('.slider input');
-  const img = document.querySelector('.images .img-2');
-  const dragLine = document.querySelector('.slider .drag-line');
+// document.addEventListener('DOMContentLoaded', function() {
+//   const slider = document.querySelector('.slider input');
+//   const img = document.querySelector('.images .img-2');
+//   const dragLine = document.querySelector('.slider .drag-line');
 
-  if (slider) {
-    slider.oninput = () => {
-      let sliderVar = slider.value;
-      dragLine.style.left = sliderVar + "%";
-      img.style.width = sliderVar + "%";
-    };
-  }
-});
+//   if (slider) {
+//     slider.oninput = () => {
+//       let sliderVar = slider.value;
+//       dragLine.style.left = sliderVar + "%";
+//       img.style.width = sliderVar + "%";
+//     };
+//   }
+// });
 
 
 
@@ -73,42 +73,56 @@ document.addEventListener('DOMContentLoaded', function() {
 // slider
 
 
-const container = document.querySelector('.wrapper');
-document.querySelector('.slider').addEventListener('input', (e) => {
-  container.style.setProperty('--position', `${e.target.value}%`);
-})
+// const container = document.querySelector('.wrapper');
+// document.querySelector('.slider').addEventListener('input', (e) => {
+//   container.style.setProperty('--position', `${e.target.value}%`);
+// })
+
+document.addEventListener('DOMContentLoaded', function () {
+  const container = document.querySelector('.wrapper');
+
+  if (container) {
+    const slider = container.querySelector('.slider');
+
+    if (slider) {
+      slider.addEventListener('input', (e) => {
+        container.style.setProperty('--position', `${e.target.value}%`);
+      });
+    }
+  }
+});
 
 
 
 
 
-// document.addEventListener('DOMContentLoaded', function() {
-//   var openModalBtn = document.getElementById('openModalBtn');
-//   var nameInput = document.getElementById('name');
-//   var phoneInput = document.getElementById('phone');
-//   var cityInput = document.getElementById('city');
-//   var modal = document.getElementById('modalPopUp');
-//   var closeModalBtn = document.getElementById('closeModalBtn');
+document.addEventListener('DOMContentLoaded', function() {
+  var openModalBtn = document.getElementById('openModalBtn');
+  var nameInput = document.getElementById('name');
+  var phoneInput = document.getElementById('phone');
+  var cityInput = document.getElementById('city');
+  var modal = document.getElementById('modalPopUp');
+  var closeModalBtn = document.getElementById('closeModalBtn');
 
-//   openModalBtn.addEventListener('click', function(event) {
-//     event.preventDefault(); 
+  openModalBtn.addEventListener('click', function(event) {
+    event.preventDefault(); 
 
    
-//     if (nameInput.value.trim() !== '' && phoneInput.value.trim() !== '' && cityInput.value !== '') {
-//       modal.style.display = 'block';
-//     } else {
+    if (nameInput.value.trim() !== '' && phoneInput.value.trim() !== '' && cityInput.value !== '') {
+      modal.style.display = 'block';
+    } else {
       
-//       alert('Please fill in all required fields.');
-//     }
-//   });
+      alert('Please fill in all required fields.');
+    }
+  });
 
-//   closeModalBtn.onclick = function() {
-//     modal.style.display = 'none';
-//   };
+  closeModalBtn.onclick = function() {
+    modal.style.display = 'none';
+  };
 
-//   window.onclick = function(event) {
-//     if (event.target === modal) {
-//       modal.style.display = 'none';
-//     }
-//   };
-// });
+  window.onclick = function(event) {
+    if (event.target === modal) {
+      modal.style.display = 'none';
+    }
+  };
+});
